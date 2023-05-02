f7f1459464ddc5ac5b2ea785d27a9b25938a77ab
WARUD <warudssss@gmail.com>
2023-05-02 11:16:12 +0700

    tambahan dengan validasi login sederhana menggunakan if else

--- main js.js
+++ main js.js
@@ -1,7 +1,17 @@
 function checkUser (username, pw) {
-    var usernameDB = "abcdef";
-    var pwDB = "123";
+    var usernameDB = "angga1";
+    var pwDB = "123w4";
     var validasi = username == usernameDB && pw == pwDB;
 
-    alert(validasi);
+    if(username == usernameDB) {
+        if(pw == pwDB) {
+            alert("berhasil login");
+        } else {
+            alert("password salah");
+        }
+        alert("Username sesuai");
+    } else {
+        alert("Username salah");
+    }
+   
 }
